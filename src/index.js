40135e6a296114faced60c81b16e4a2d7a6fdc8c
WilcoApp <hello@wilco.gg>
2025-11-10 11:44:50 +0000

    Add main entry point that uses calculator module

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const calculator = require('./calculator');
+
+console.log('Welcome to the CI/CD Demo Project!');
+console.log('Calculator demo:');
+console.log('2 + 3 =', calculator.add(2, 3));
+console.log('4 * 5 =', calculator.multiply(4, 5));
+console.log('10 / 2 =', calculator.divide(10, 2));
